0fb70ab4d66d4b789fad6424fb5d06d57265708e
namrahov <nurlan.amrahov@pashabank.az>
2021-08-05 20:51:14 +0400

    sdsd

--- service/salary/salaryService.go
+++ service/salary/salaryService.go
@@ -2,30 +2,20 @@ package salary
 
 import (
 	log "github.com/sirupsen/logrus"
+	"math/big"
 )
 
 type Service struct {
 }
 
 type IService interface {
-	ConvertGrossToNet(id int) (*model.EmployeeDto, *model.ErrorResponse)
+	ConvertGrossToNet() big.Int
 }
 
-func (s *Service) ConvertGrossToNet(id int) (*model.EmployeeDto, *model.ErrorResponse) {
+func (s *Service) ConvertGrossToNet() float64 {
 	log.Info("ActionLog.GetEmployee.start")
-
-	employeeList := []model.EmployeeDto{
-		{1, `Nurlan`},
-		{2, `Hesen`},
-	}
-
-	for _, v := range employeeList {
-		if v.Id == id {
-			return &v, nil
-		}
-	}
 
 	log.Info("ActionLog.GetEmployee.success")
 
-	return nil, nil
+	return 2.4
 }
