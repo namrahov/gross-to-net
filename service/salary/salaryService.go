d9af26a2bceed2f151cc0c53b3766376ef656660
namrahov <nurlan.amrahov@pashabank.az>
2021-08-05 21:13:50 +0400

    ddd

--- service/salary/salaryService.go
+++ service/salary/salaryService.go
@@ -1,1 +1,18 @@
 package salary
+
+import log "github.com/sirupsen/logrus"
+
+type Service struct {
+}
+
+type IService interface {
+	GetEmployee() float64
+}
+
+func (s *Service) GetEmployee() float64 {
+	log.Info("ActionLog.GetEmployee.start")
+
+	log.Info("ActionLog.GetEmployee.success")
+
+	return 2.54
+}
